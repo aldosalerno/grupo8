2860bb018fb8e55829dc95b7270455e0f1bc5001
DamjoCAT <tdanit3238@gmail.com>
2024-07-11 20:49:33 -0300

    update y get arreglados

--- src/controllers/usuariosController.js
+++ src/controllers/usuariosController.js
@@ -1,69 +1,23 @@
 const db = require('../db/db');
-const jwt = require('jsonwebtoken');
-const { promisify } = require('util');
 
 
+const updateUsuario = (req, res) => {
 
-
-const selectUsuarioIncompleto = (req, res) => {
     const token =  req.cookies.jwt;
     function decodeJWT(token) {
     return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
     }
-  
+
     const decodificada = decodeJWT(token);
-    console.log(decodificada.id);  
 
-    console.log(decodificada.id + " incompleto");
-
-
-    const sql = 'SELECT * FROM USUARIOS where usuario_ID = ?';
-    db.query(sql, [decodificada.id], (err, results) => {
-        if (err) throw err;
-        console.log(results);
-        res.render("usuarios", {
-            Username: results[0].usuario_USERNAME, 
-            Email: results[0].usuario_EMAIL, 
-            Nombre: "", 
-            Apellido: "", 
-            Date: ""});
-     });
-};
-
-const selectUsuario = (req, res) => {
-    const id = req.params.id;
-
-    const sql = 'SELECT * FROM USUARIOS JOIN USUARIOS_INFO on USUARIOS.usuario_ID = USUARIOS_INFO.usuario_ID AND USUARIOS.usuario_ID = ?;';
-
-
-     db.query(sql, [id], (err, results) => {
-         if (err) throw err;
-
-         const boleanogil =  results ?? "" ;
-         
-            if(boleanogil.length == 0){
-               res.redirect('/usuarios/mostrar/incompleto/');
-            } else {
-                res.render("usuarios", {
-                    Username: results[0].usuario_USERNAME, 
-                    Email: results[0].usuario_EMAIL, 
-                    Nombre: results[0].info_NAME, 
-                    Apellido: results[0].info_LASTNAME, 
-                    Date: results[0].info_YEARBIRTH });
-            }
-             
-        });
-};
-
-const updateUsuario = (req, res) => {
-  const id = req.params.id;
+  
   const { nombre, lastname, number } = req.body;
   
-  console.log(id, nombre, lastname, number);
+  console.log(decodificada.id, nombre, lastname, number);
 
   const sql = 'UPDATE USUARIOS_INFO SET info_NAME = ?, info_LASTNAME = ?, info_YEARBIRTH = ? WHERE usuario_ID = ?';
 
-  db.query(sql, [nombre, lastname, number, id], (err, results) => {
+  db.query(sql, [nombre, lastname, number, decodificada.id], (err, results) => {
       if (err) throw err;
       console.log(results);
       res.redirect('/usuarios/mostrar/');  
@@ -71,12 +25,19 @@ const updateUsuario = (req, res) => {
 };
 
 const deleteUsuario = (req, res) => {
-    const id = req.params.id;
 
-    console.log(id);
+    const token =  req.cookies.jwt;
+    function decodeJWT(token) {
+    return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
+    }
+
+    const decodificada = decodeJWT(token);
+
+
+    console.log(decodificada.id);
 
     const sql = 'DELETE FROM USUARIOS WHERE usuario_ID = ?';
-    db.query(sql, [id], (err, results) => {
+    db.query(sql, [decodificada.id], (err, results) => {
         if (err) throw err;
         res.json({ message: 'Usuario eliminado' });
     });
@@ -100,7 +61,7 @@ const deleteUsuario = (req, res) => {
     
  }
 
-const usuarios = (req, res) => {
+const selectUsuarios = (req, res) => {
    
         const token =  req.cookies.jwt;
         function decodeJWT(token) {
@@ -115,9 +76,22 @@ const usuarios = (req, res) => {
             if (err) throw err;
    
             const boleanogil =  results ?? "" ;
-            
+
+                
                if(boleanogil.length == 0){
-                  res.redirect('/usuarios/mostrar/incompleto/');
+
+                const sql2 = 'SELECT * FROM USUARIOS where usuario_ID = ?';
+
+                db.query(sql2, [decodificada.id], (err, results) => {
+                    if (err) throw err;
+                    console.log(results);
+                    res.render("usuarios", {
+                        Username: results[0].usuario_USERNAME, 
+                        Email: results[0].usuario_EMAIL, 
+                        Nombre: "", 
+                        Apellido: "", 
+                        Date: ""});
+                 });
                } else {
                    res.render("usuarios", {
                        Username: results[0].usuario_USERNAME, 
@@ -135,9 +109,7 @@ const usuarios = (req, res) => {
 
 
  module.exports = {
-   usuarios,
-   selectUsuario,
-   selectUsuarioIncompleto,
+   selectUsuarios,
    updateUsuario, 
    deleteUsuario, 
    usuarioLogin, 
--- src/routes/usuariosRouter.js
+++ src/routes/usuariosRouter.js
@@ -4,9 +4,7 @@ const authController = require('../controllers/authController');
 const controller = require('../controllers/usuariosController');
 
 
-router.get("/", authController.isAuthenticated, controller.usuarios);
-router.get(`/mostrar/:id`, controller.selectUsuario);
-router.get(`/mostrar/incompleto/:id`, controller.selectUsuarioIncompleto);  
+router.get("/", authController.isAuthenticated, controller.selectUsuarios); 
 router.post(`/update/:id`, controller.updateUsuario);
 router.delete(`/delete/:id`, controller.deleteUsuario);
 
